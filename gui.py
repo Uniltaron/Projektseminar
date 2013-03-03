fbd07db84f1c0d30d407b16c51c653d2abdfaf4f
Jan Scheddler <Jan.Scheddler@uni-duesseldorf.de>
2013-03-03 04:08:21 +0100

    GUI-Yay!
    First shot at gui, nothing special yet

---
+++ gui.py
@@ -0,0 +1,34 @@
+from Tkinter import *
+#OOP is just what we need!
+class Gui(object):
+    def __init__(self, master):
+        #setup mainframe, contains all gui-elements
+        frame = Frame(master)
+        frame.grid() #grid > pack
+        
+        #"New Thesaurus"-Button
+        self.new_thb = Button (frame, text="New Thesaurus", command=self.new_the)
+        self.new_thb.grid(row = 0, column = 0)
+        
+        #"Import Thesaurus"-Button
+        self.import_thb = Button (frame, text="Import Thesaurus", command = self.import_the)
+        self.import_thb.grid(row = 0, column = 1)
+        
+        #"Quit"-Button
+        self.endb = Button (frame, text="Quit", command = frame.quit)
+        self.endb.grid(row=1, column = 0)
+    
+    #button1 command
+    def new_the(self):
+        print "Erstellt irgendwann einen neuen Thesaurus!"
+    
+    #button2 command
+    def import_the(self):
+        print "Importiert irgendwann mal einen bestehenden Thesaurus!"
+        
+        
+#basic gui stuff        
+root = Tk()
+gui = Gui(root)
+
+root.mainloop()
