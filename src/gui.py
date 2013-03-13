301f94d9779b8dcd396ea40ccd69f8a914cad3b6
Jan Scheddler <Jan.Scheddler@uni-duesseldorf.de>
2013-03-13 13:37:30 +0100

    commit, damit er die Klappe hält

--- src/gui.py
+++ src/gui.py
@@ -3,11 +3,11 @@ from Tkinter import *
 
 #OOP is just what we need!
 class Gui(object):
-    def __init__(self, master):
+    def __init__(self, master, bla, fasel):
         #setup mainframe, contains all gui-elements
         frame = Frame(master)
         frame.grid() #grid > pack
-        
+        self.switch = True
         #"New Thesaurus"-Button
         self.new_thb = Button (frame, text="New Thesaurus", command=self.new_the)
         self.new_thb.grid(row = 0, column = 0)
@@ -19,16 +19,39 @@ class Gui(object):
         #"Quit"-Button
         self.endb = Button (frame, text="Quit", command = frame.quit)
         self.endb.grid(row=1)
-    
+        
+        #test:Button to blend out the new_thb:
+        self.blend_out_b = Button (frame, text="Ausblenden", command = self.blend_out)
+        self.blend_out_b.grid(row=2)
+        
+        #Create Function-Objects
+        self.button_func = Gui_Funcs(bla, fasel)
     #button1 command
     def new_the(self):
-        print "Erstellt irgendwann einen neuen Thesaurus!"
+        self.button_func._bla_()
     
     #button2 command
     def import_the(self):
-        print "Importiert irgendwann mal einen bestehenden Thesaurus!"
-        
+        self.button_func._blu_()
+    def blend_out(self):
+        self.import_thb.grid_remove()
+    
+
+#just testing some things here...
+class Gui_Funcs(object):
+    def __init__(self, bla, fasel):
+        self.bla = bla
+        self.fasel = fasel
+    def _bla_(self):
+        print "bla"
+        i = 0
+        while i != self.fasel:
+            print self.bla
+            i += 1
+    def _blu_(self):
+        print "blu"
+
 root = Tk()
-app = Gui(root)
+app = Gui(root, "bla", 2)
 
 root.mainloop()
