1ab26494e35ebd509e20e15e058cf20740714a8f
liesa witt <liesa.witt@uni-duesseldorf.de>
2013-03-16 11:07:27 +0100

    veränderungen im Im/Exprot und Relations

--- Im_Ex.py
+++ Im_Ex.py
@@ -14,16 +14,48 @@ import csv, sys
     writer.writerows(list_data)
     
     
+    
 import json
 	with open("some.json", "r") as json_input:
     data = json.load(json_input)
 		print data, type(data)
+		
+new_data={}
+
 	with open("new_some.json", "w") as json_output:
     json.dump(new_data, json_output)		
 		
 		
-		
-		
+			
 import xml.etree.ElementTree as ET
-tree = ET.parse('some.xml')
-root = tree.getroot() 
+tree = ET.parse('some.xml')						
+root = tree.getroot() 							 #Wurzel-Element xml holen
+if root != 'concepts':
+	print 'falsches Format'
+
+else:	
+
+print root										 # Objekt an Speicherstelle
+print root.tag
+print root.attrib 								 # Attribute, die zum Tag gehoeren
+print root.getchildren() 						# alle Kinderknoten auf der naechsten Ebene
+												# Pruefen, ob es weitere Kinderknoten gibt:
+												
+												if root.getchildren():
+    print "Es gibt weitere Kinderknoten"
+else:
+    print "Keine weitere Kinderknoten vorhanden"
+for child in root:
+    print child.tag, child.attrib, child.text
+    
+words = root.getchildren()
+
+for descriptor in descriptoren:
+    discriptor.tag, discriptor, attrib
+
+for descriptor in root.findall('descriptor'):					#jedes "word finden"
+    rank = descriptor.find('rank').text						# zu jedem word ein rank finden
+    name = descriptor.get('name')
+    print name, rank    
+    
+
--- relationen.py
+++ relationen.py
@@ -1,13 +1,25 @@
 class Relations(object):
 	 def __init__(self, name, BF, BS ,OB, UB,VB, SB ): # ENGL BEDEUTUNGEN !!!!
-	 	self._BF = Benutzt fuer # ausschreiben, ja oder nein
+	 	self._BF = Benutzt fuer 
 	 	self._BS = Benutztes Synonym
 	 	self._OB = Oberbegriff
 	 	self._UB = Unterbegriff
 	 	self._VB = Verwandter Begriff
 	 	self._SB = Spitzenbegriff
-def set name
-def 
-	 DEF_ADD_UB
-	 .....
+	 	
+def set name(self):
+	def add_BF 
+	def add_BS
+	def add_OB
+	def add_UB
+	def add_VB
+	def add_SB
+
+#oder : 
+# def add_relations(begriff, relations)	
+#	relations=relations
+#	self.relations.append(Begriff)
+
+	
+	 
 	 
