c2241be86d25bdf344515412847642c324b69b30
Jan Scheddler <Jan.Scheddler@uni-duesseldorf.de>
2013-04-01 01:27:55 +0200

    Konrads Änderungen...
    Wird schon XD

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 #Projektseminar Wissensrepräsentation
-#Konrad Kowalke, Jan Simon Scheddler, Liesa Witt, Michael Weidauer
+#Konrad Kowalke, Jan Simon Scheddler, Michael Weidauer, Liesa Witt
 # -*- coding: utf-8 -*-
 
-thesaurus = {}
+# Thesaurus befindet sich innerhalb der Klasse Deskriptorsatz
+# Deskriptors�tze werden anhand dessen key im dictionary 'Deskriptorsatz.thesaurus' aufgerufen
 
-class deskriptorsatz(object):
-
-
+class Deskriptorsatz(object):
+    thesaurus = {}
 # Legt einen neuen Deskriptorsatz an, dieser enthält noch keine Relationen
-# Überprüft zuerst, ob es sich bei der Eingabe um einen String handelt 
-# Erlaubt jedoch auch Eingaben wie '23'
+# Überprüft zuerst, ob es sich beim Namen des Deskriptorsatzes um einen String handelt 
+# Die Instanz wird dem dictionary 'thesaurus' unter dessen Name hinzugef�gt
+# Bsp.: Deskriptorsatz('Hund')      legt einen neuen Deskriptorsatz Hund an und speichert diesen im dictionary Deskriptorsatz.thesaurus 
 
     def __init__(self,deskr):
         try:
             if not isinstance(deskr,str):
                 raise Exception()
-            #deskr = deskr.capitalize()
+            deskr = deskr.capitalize()
             self.deskr=deskr
             self.bf=[]
             self.bs=[]
@@ -23,20 +24,18 @@ class deskriptorsatz(object):
             self.ub=[]
             self.vb=[]
             self.sb=[]
-            thesaurus[deskr]=self
+            Deskriptorsatz.thesaurus[deskr]=self
+            print "Der Deskriptorsatz '%s' wurde neu angelegt!\n" % deskr
         except:
             print"Der Deskriptorsatz konnte nicht angelegt werden!\n"
 
-
+# L�scht einen Deskriptorsatz
+# Allderdings verschwindet die Instanz erst dann ganz, wenn alle Referenzen auf diese Instanz gel�scht wurden
     def __del__(self):
         try:
-            del thesaurus[deskr]
+            del Deskriptorsatz.thesaurus[deskr]
         except:
-            print "Es ist ein Fehler beim Löschen aufgetreten!"
-
-
-    def delete_deskriptorsatz(self):
-        self = None
+            print "Es ist ein Fehler beim Löschen aufgetreten!\nEntfernen Sie erst alle anderen Referenzen!"
     
 # Gibt einen Deskriptorsatz mitsamt seiner Relationen aus
 
@@ -48,68 +47,76 @@ class deskriptorsatz(object):
 
 
 
-# Fügt einem Deskriptorsatz einen neuen Term zu einer beliebigen Relation hinzu
-# Überprüft zuerst, ob es sich bei der Eingabe um einen String handelt
+# F�gt einem Deskriptorsatz die Instanz eines eingegebenen Terms als Relationen hinzu
+# Erfordert, dass der Term bereits als Deskriptorsatz angelegt wurde
+# �berpr�ft zuerst, ob es sich bei der Eingabe um einen String handelt
+# F�gt im Umkehrschluss der Instanz des eingegebenen Terms die Instanz des Diskriptorsatzes hinzu 
 
     def add_relation(self,term,relation):
         try:
-           # term = term.capitalize()
-            #relation = relation.upper()
+            term = term.capitalize()
+            relation = relation.upper()
             if (not isinstance(term,str)) and (not isinstance(relation,str)):
                 raise Exception()
-            elif (term is self.deskr):
+            elif (term == self.deskr):
                 print "Ein Deskriptor kann sich nicht selbst zur Relation haben!\n"
             elif (self.bs):
                 print "Ein Nichtdeskriptor kann keine Relationen erhalten!\n"
+            elif (term not in Deskriptorsatz.thesaurus):
+                print "Bitte zuerst '%s' als Deskriptorsatz anlegen!\n" % term
             else:
                 if (relation == 'BF'):
-                    if (term in self.bf):
+                    if (Deskriptorsatz.thesaurus[term] in self.bf):
                         print "'%s' ist bereits im Deskriptorsatz enthalten!\n" % term
                     else:
-                        self.bf.append(term)
+                        self.bf.append(Deskriptorsatz.thesaurus[term])
                         print "Dem Deskriptorsatz wurde '%s' als neues Synonym hinzugefügt!\n" % term
+                        Deskriptorsatz.thesaurus[term].bs.append(self)
 
                 elif (relation == 'BS'):
-                    if (term in self.bs):
+                    if (Deskriptorsatz.thesaurus[term] in self.bs):
                         print "'%s' ist bereits im Deskriptorsatz enthalten!\n" % term
                     elif (self.bf or self.ob or self.ub or self.vb or self.sb):
                         print "'%s' hat bereits andere Relationen und kann daher kein Nicht-Deskriptor sein!\n" % term
                     else:
-                        self.bs.append(term)
+                        self.bs.append(Deskriptorsatz.thesaurus[term])
                         print "Dem Deskriptorsatz wurde '%s' als neues Symonym hinzugefügt!\n'%s' ist jetzt ein Nicht-Deskriptor!\n" % term, term
                         
                 elif (relation == 'OB'):
                     if (self.ob):
                         print "Der Deskriptor hat bereits einen Oberbegriff!\n"
-                    elif (term in self.ob):
+                    elif (Deskriptorsatz.thesaurus[term] in self.ob):
                         print "'%s' ist bereits im Deskriptorsatz enthalten!\n" % term
-                    elif (term in self.ub):
+                    elif (Deskriptorsatz.thesaurus[term] in self.ub):
                         print "'%s' ist bereits Unterbegriff des Deskriptors!\n" % term
                     else:
-                        self.ob.append(term)
+                        self.ob.append(Deskriptorsatz.thesaurus[term])
                         print "Dem Deskriptorsatz wurde '%s' als neuer Oberbegriff hinzugefügt!\n" % term
+                        Deskriptorsatz.thesaurus[term].ub.append(self)
 
                 elif (relation == 'UB'):
-                    if (term in self.ub):
+                    if (Deskriptorsatz.thesaurus[term] in self.ub):
                         print "'%s' ist bereits im Deskriptorsatz enthalten!\n" % term
-                    elif (term in self.ob):
+                    elif (Deskriptorsatz.thesaurus[term] in self.ob):
                         print "'%s' ist bereits Oberbegriff des Deskriptors!" % term
                     else:
-                        self.ub.append(term)
+                        self.ub.append(Deskriptorsatz.thesaurus[term])
                         print "Dem Deskriptorsatz wurde '%s' als neuer Unterbegriff hinzugefügt!\n" % term
+                        Deskriptorsatz.thesaurus[term].ob.append(self)
 
                 elif (relation == 'VB'):
-                    if (term in self.vb):
+                    if (Deskriptorsatz.thesaurus[term] in self.vb):
                         print "'%s' ist bereits im Deskriptorsatz enthalten!\n" % term
                     else:
-                        self.vb.append(term)
+                        self.vb.append(Deskriptorsatz.thesaurus[term])
                         print "Dem Deskriptorsatz wurde '%s' als neuer verwandter Begriff hinzugefügt!\n" % term
+                        Deskriptorsatz.thesaurus[term].vb.append(self)
 
                 elif (relation == 'SB'):
-                    if (term in self.sb):
+                    if (Deskriptorsatz.thesaurus[term] in self.sb):
                         print "'%s' ist bereits im Deskriptorsatz enthalten!\n" % term
                     else:
-                        self.sb.append(term)
+                        self.sb.append(Deskriptorsatz.thesaurus[term])
                         print "Dem Deskriptorsatz wurde '%s' als neuer Spitzenbegriff hinzugefügt!\n" % term
 
                 else:
@@ -119,80 +126,87 @@ class deskriptorsatz(object):
 
 
 
-# Entfernt einen Term aus einer beliebigen Relation
+# Entfernt einen die Instanz eines eingegebenen Terms aus einer beliebigen Relation
 # Überprüft zuerst, ob es sich bei der Eingabe um einen String handelt
+# Entfernt im Umkehrschluss die Instanz des Deskriptorsatzes aus der Instanz des Terms
 
     def delete_relation(self,term,relation):
         try:
-            #term = term.capitalize()
-            #relation = relation.upper() 
+            term = term.capitalize()
+            relation = relation.upper() 
             if (not isinstance(term,str)) and (not isinstance(relation,str)):
                 raise Exception()
             else:
-                if (relation is 'BF'):
-                    if (term not in self.bf):
+                if (relation == 'BF'):
+                    if (Deskriptorsatz.thesaurus[term] not in self.bf):
                         print "'%s' ist unter der Relation '%s' nicht zu finden!\n" % (term, relation)
                     else:
-                        self.bf.remove(term)
+                        self.bf.remove(Deskriptorsatz.thesaurus[term])
                         print "'%s' wurde erfolgreich entfernt!\n" % term
+                        Deskriptorsatz.thesaurus[term].bs.remove(self)
 
-                elif (relation is 'BS'):
-                    if (term not in self.bs):
+                elif (relation == 'BS'):
+                    if (Deskriptorsatz.thesaurus[term] not in self.bs):
                         print "'%s' ist unter der Relation '%s' nicht zu finden!\n" % (term, relation)
                     else:
-                        self.bs.remove(term)
-
-                elif (relation is 'OB'):
-                    if (term not in self.ob):
-                        print "'%s' ist unter der Relation '%s' nicht zu finden!\n" % (term, relation)
-                    else:
-                        self.ob.remove(term)
+                        self.bs.remove(Deskriptorsatz.thesaurus[term])
                         print "'%s' wurde erfolgreich entfernt!\n" % term
+                        Deskriptorsatz.thesaurus[term].bf.remove(self)
 
-                elif (relation is 'UB'):
-                    if (term not in self.ub):
+                elif (relation == 'OB'):
+                    if (Deskriptorsatz.thesaurus[term] not in self.ob):
                         print "'%s' ist unter der Relation '%s' nicht zu finden!\n" % (term, relation)
                     else:
-                        self.ub.remove(term)
+                        self.ob.remove(Deskriptorsatz.thesaurus[term])
                         print "'%s' wurde erfolgreich entfernt!\n" % term
+                        Deskriptorsatz.thesaurus[term].ub.remove(self)
 
-                elif (relation is 'VB'):
-                    if (term not in self.vb):
+                elif (relation == 'UB'):
+                    if (Deskriptorsatz.thesaurus[term] not in self.ub):
                         print "'%s' ist unter der Relation '%s' nicht zu finden!\n" % (term, relation)
                     else:
-                        self.vb.remove(term)
+                        self.ub.remove(Deskriptorsatz.thesaurus[term])
                         print "'%s' wurde erfolgreich entfernt!\n" % term
+                        Deskriptorsatz.thesaurus[term].ob.remove(self)
 
-                elif (relation is 'SB'):
-                    if (term not in self.sb):
+                elif (relation == 'VB'):
+                    if (Deskriptorsatz.thesaurus[term] not in self.vb):
                         print "'%s' ist unter der Relation '%s' nicht zu finden!\n" % (term, relation)
                     else:
-                        self.sb.remove(term)
+                        self.vb.remove(Deskriptorsatz.thesaurus[term])
+                        print "'%s' wurde erfolgreich entfernt!\n" % term
+                        Deskriptorsatz.thesaurus[term].vb.remove(self)
+
+                elif (relation == 'SB'):
+                    if (Deskriptorsatz.thesaurus[term] not in self.sb):
+                        print "'%s' ist unter der Relation '%s' nicht zu finden!\n" % (term, relation)
+                    else:
+                        self.sb.remove(Deskriptorsatz.thesaurus[term])
                         print "'%s' wurde erfolgreich entfernt!\n" % term
         except:
-            print "Beim Löschen des Terms ist ein Fehler aufgetreten!\n"
+            print "Beim L�schen des Terms ist ein Fehler aufgetreten!\n"
 
 
-def suche_thesaurus(term):
-    try:
-        if thesaurus.has_key(term):
-            print "success"
-            term.ausgabe() 
-            
-    except:
-        print "Fehler bei der Suche"
-    
-
-
+    def suche(term):
+        pass
 
 #****************Test****************#
 
-if __name__ == '__main__':
-    Hund = deskriptorsatz('Hund')
-    Katze = deskriptorsatz('Katze')
-    Tier = deskriptorsatz('Tier')
+'''if __name__ == '__main__':
+    Hund = Deskriptorsatz('hund')
+    Katze = Deskriptorsatz('Katze')
+    Tier = Deskriptorsatz('Tier')
+    Pudel = Deskriptorsatz('Pudel')
     Hund.add_relation('Saeuegetier','OB')
-    Hund.add_relation('Pudel','UB')
-    Hund.add_relation('Dackel','UB')
+    Hund.add_relation('pudel','UB')
+    Hund.add_relation('DACKEL','UB')
     Hund.add_relation('Hundefutter','VB')
     Hund.add_relation('Raubtier','OB')
+'''
+
+Deskriptorsatz('Hund')
+Deskriptorsatz('Pudel')
+Deskriptorsatz.thesaurus['Hund'].add_relation('Pudel','ub')
+
+
+
